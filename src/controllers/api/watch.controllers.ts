549125e319c5dd7779484552bc49ff80f8634ce8
namdh03 <duonghoangnam503@gmail.com>
2024-06-25 00:45:46 +0700

    feat: api create watch

--- src/controllers/api/watch.controllers.ts
+++ src/controllers/api/watch.controllers.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import { WATCH_MESSAGES } from '~/constants/messages'
+import { TypedRequestBody } from '~/models/requests'
+import { WatchReqBody } from '~/models/requests/Watch.requests'
 import watchService from '~/services/watch.service'
 
 // [GET] /watches
@@ -8,5 +10,14 @@ export const getWatchesController = async (_req: Request, res: Response) => {
   res.json({
     message: WATCH_MESSAGES.GET_WATCHES_SUCCESSFULLY,
     data: watches
+  })
+}
+
+// [POST] /watches
+export const createWatchController = async (req: TypedRequestBody<WatchReqBody>, res: Response) => {
+  const watch = await watchService.createWatch(req.body)
+  res.json({
+    message: WATCH_MESSAGES.CREATE_WATCH_SUCCESSFULLY,
+    data: watch
   })
 }
--- src/routes/api/watch.routes.ts
+++ src/routes/api/watch.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
-import { getWatchesController } from '~/controllers/api/watch.controllers'
+import { createWatchController, getWatchesController } from '~/controllers/api/watch.controllers'
+import { apiIsAdminMiddleware } from '~/middlewares/auth.middlewares'
+import { apiAccessTokenValidator } from '~/middlewares/user.middlewares'
+import { bodyWatchValidator } from '~/middlewares/watch.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const watchRouter = Router()
@@ -10,5 +13,26 @@ const watchRouter = Router()
  * Method: GET
  */
 watchRouter.get('/', wrapRequestHandler(getWatchesController))
+
+/**
+ * Description. Create watch
+ * Path: /watches
+ * Method: POST
+ * Body: {
+ *  watchName: string
+ *  image: string
+ *  price: number
+ *  automatic: boolean
+ *  watchDescription: string
+ *  brandId: string
+ * }
+ * */
+watchRouter.post(
+  '/',
+  apiAccessTokenValidator,
+  apiIsAdminMiddleware,
+  bodyWatchValidator,
+  wrapRequestHandler(createWatchController)
+)
 
 export default watchRouter
